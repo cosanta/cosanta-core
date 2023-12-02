1cd2a0c6b423491e4b70a2430b31bc5c56bb6b87
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2023-12-02 19:24:00 +0200

    revert cosantafication of dashstrings

--- src/qt/dashstrings.cpp
+++ src/qt/dashstrings.cpp
@@ -9,8 +9,10 @@
 #define UNUSED
 #endif
 static const char UNUSED *dash_strings[] = {
-QT_TRANSLATE_NOOP("cosanta-core", "Cosanta Core"),
 QT_TRANSLATE_NOOP("cosanta-core", "The %s developers"),
+QT_TRANSLATE_NOOP("cosanta-core", ""
+"%s corrupt. Try using the wallet tool dash-wallet to salvage or restoring a "
+"backup."),
 QT_TRANSLATE_NOOP("cosanta-core", ""
 "%s file contains all private keys from this wallet. Do not share it with "
 "anyone!"),
@@ -18,11 +20,11 @@ QT_TRANSLATE_NOOP("cosanta-core", ""
 "%s uses exact denominated amounts to send funds, you might simply need to "
 "mix some more coins."),
 QT_TRANSLATE_NOOP("cosanta-core", ""
-"-masternode option is deprecated and ignored, specifying -"
-"masternodeblsprivkey is enough to start this node as a masternode."),
-QT_TRANSLATE_NOOP("cosanta-core", ""
 "-maxtxfee is set very high! Fees this large could be paid on a single "
 "transaction."),
+QT_TRANSLATE_NOOP("cosanta-core", ""
+"Can't generate a change-address key. No keys in the internal keypool and "
+"can't generate any keys."),
 QT_TRANSLATE_NOOP("cosanta-core", ""
 "Cannot obtain a lock on data directory %s. %s is probably already running."),
 QT_TRANSLATE_NOOP("cosanta-core", ""
@@ -42,6 +44,9 @@ QT_TRANSLATE_NOOP("cosanta-core", ""
 "Failed to create backup, file already exists! This could happen if you "
 "restarted wallet in less than 60 seconds. You can continue if you are ok "
 "with this."),
+QT_TRANSLATE_NOOP("cosanta-core", ""
+"Fee estimation failed. Fallbackfee is disabled. Wait a few blocks or enable -"
+"fallbackfee."),
 QT_TRANSLATE_NOOP("cosanta-core", ""
 "Found unconfirmed denominated outputs, will wait till they confirm to "
 "continue."),
@@ -78,6 +83,10 @@ QT_TRANSLATE_NOOP("cosanta-core", ""
 QT_TRANSLATE_NOOP("cosanta-core", ""
 "The transaction amount is too small to send after the fee has been deducted"),
 QT_TRANSLATE_NOOP("cosanta-core", ""
+"This error could occur if this wallet was not shutdown cleanly and was last "
+"loaded using a build with a newer version of Berkeley DB. If so, please use "
+"the software that last loaded this wallet"),
+QT_TRANSLATE_NOOP("cosanta-core", ""
 "This is a pre-release test build - use at your own risk - do not use for "
 "mining or merchant applications"),
 QT_TRANSLATE_NOOP("cosanta-core", ""
@@ -100,26 +109,15 @@ QT_TRANSLATE_NOOP("cosanta-core", ""
 "Unable to replay blocks. You will need to rebuild the database using -"
 "reindex-chainstate."),
 QT_TRANSLATE_NOOP("cosanta-core", ""
-"Unsupported argument -socks found. Setting SOCKS version isn't possible "
-"anymore, only SOCKS5 proxies are supported."),
-QT_TRANSLATE_NOOP("cosanta-core", ""
-"Unsupported argument -whitelistalwaysrelay ignored, use -whitelistrelay and/"
-"or -whitelistforcerelay."),
-QT_TRANSLATE_NOOP("cosanta-core", ""
 "WARNING! Failed to replenish keypool, please unlock your wallet to do so."),
 QT_TRANSLATE_NOOP("cosanta-core", ""
 "Wallet is locked, can't replenish keypool! Automatic backups and mixing are "
 "disabled, please unlock your wallet to replenish keypool."),
 QT_TRANSLATE_NOOP("cosanta-core", ""
+"Warning: Private keys detected in wallet {%s} with disabled private keys"),
+QT_TRANSLATE_NOOP("cosanta-core", ""
 "Warning: The network does not appear to fully agree! Some miners appear to "
 "be experiencing issues."),
-QT_TRANSLATE_NOOP("cosanta-core", ""
-"Warning: Unknown block versions being mined! It's possible unknown rules are "
-"in effect"),
-QT_TRANSLATE_NOOP("cosanta-core", ""
-"Warning: Wallet file corrupt, data salvaged! Original %s saved as %s in %s; "
-"if your balance or transactions are incorrect you should restore from a "
-"backup."),
 QT_TRANSLATE_NOOP("cosanta-core", ""
 "Warning: We do not appear to fully agree with our peers! You may need to "
 "upgrade, or other nodes may need to upgrade."),
@@ -130,17 +128,17 @@ QT_TRANSLATE_NOOP("cosanta-core", ""
 "mode.  This will redownload the entire blockchain"),
 QT_TRANSLATE_NOOP("cosanta-core", "%d of last 100 blocks have unexpected version"),
 QT_TRANSLATE_NOOP("cosanta-core", "%s can't be lower than %s"),
-QT_TRANSLATE_NOOP("cosanta-core", "%s corrupt, salvage failed"),
 QT_TRANSLATE_NOOP("cosanta-core", "%s failed"),
 QT_TRANSLATE_NOOP("cosanta-core", "%s is idle."),
 QT_TRANSLATE_NOOP("cosanta-core", "%s is not a valid backup folder!"),
+QT_TRANSLATE_NOOP("cosanta-core", "%s is only allowed with a single wallet file"),
 QT_TRANSLATE_NOOP("cosanta-core", "%s is set very high!"),
-QT_TRANSLATE_NOOP("cosanta-core", "%s request incomplete: %s"),
+QT_TRANSLATE_NOOP("cosanta-core", "%s request incomplete:"),
 QT_TRANSLATE_NOOP("cosanta-core", "-devnet can only be specified once"),
-QT_TRANSLATE_NOOP("cosanta-core", "-litemode is deprecated."),
 QT_TRANSLATE_NOOP("cosanta-core", "-maxmempool must be at least %d MB"),
 QT_TRANSLATE_NOOP("cosanta-core", "-port must be specified when -devnet and -listen are specified"),
 QT_TRANSLATE_NOOP("cosanta-core", "-rpcport must be specified when -devnet and -server are specified"),
+QT_TRANSLATE_NOOP("cosanta-core", "A fatal internal error occurred, see debug.log for details"),
 QT_TRANSLATE_NOOP("cosanta-core", "Already have that input."),
 QT_TRANSLATE_NOOP("cosanta-core", "Automatic backups disabled"),
 QT_TRANSLATE_NOOP("cosanta-core", "Can't find random Masternode."),
@@ -148,11 +146,16 @@ QT_TRANSLATE_NOOP("cosanta-core", "Can't mix while sync in progress."),
 QT_TRANSLATE_NOOP("cosanta-core", "Can't mix: no compatible inputs found!"),
 QT_TRANSLATE_NOOP("cosanta-core", "Cannot downgrade wallet"),
 QT_TRANSLATE_NOOP("cosanta-core", "Cannot resolve -%s address: '%s'"),
+QT_TRANSLATE_NOOP("cosanta-core", "Cannot set -peerblockfilters without -blockfilterindex."),
 QT_TRANSLATE_NOOP("cosanta-core", "Cannot write to data directory '%s'; check permissions."),
 QT_TRANSLATE_NOOP("cosanta-core", "Change index out of range"),
 QT_TRANSLATE_NOOP("cosanta-core", "Collateral not valid."),
+QT_TRANSLATE_NOOP("cosanta-core", "Config setting for %s only applied on %s network when in [%s] section."),
 QT_TRANSLATE_NOOP("cosanta-core", "Copyright (C)"),
 QT_TRANSLATE_NOOP("cosanta-core", "Corrupted block database detected"),
+QT_TRANSLATE_NOOP("cosanta-core", "Could not find asmap file %s"),
+QT_TRANSLATE_NOOP("cosanta-core", "Could not parse asmap file %s"),
+QT_TRANSLATE_NOOP("cosanta-core", "Disk space is too low!"),
 QT_TRANSLATE_NOOP("cosanta-core", "Do you want to rebuild the block database now?"),
 QT_TRANSLATE_NOOP("cosanta-core", "Done loading"),
 QT_TRANSLATE_NOOP("cosanta-core", "ERROR! Failed to create automatic backup"),
@@ -161,6 +164,7 @@ QT_TRANSLATE_NOOP("cosanta-core", "Entry exceeds maximum size."),
 QT_TRANSLATE_NOOP("cosanta-core", "Error initializing block database"),
 QT_TRANSLATE_NOOP("cosanta-core", "Error initializing wallet database environment %s!"),
 QT_TRANSLATE_NOOP("cosanta-core", "Error loading %s"),
+QT_TRANSLATE_NOOP("cosanta-core", "Error loading %s: Private keys can only be disabled during creation"),
 QT_TRANSLATE_NOOP("cosanta-core", "Error loading %s: Wallet corrupted"),
 QT_TRANSLATE_NOOP("cosanta-core", "Error loading %s: Wallet requires newer version of %s"),
 QT_TRANSLATE_NOOP("cosanta-core", "Error loading %s: You can't disable HD on an already existing HD wallet"),
@@ -170,25 +174,24 @@ QT_TRANSLATE_NOOP("cosanta-core", "Error opening block database"),
 QT_TRANSLATE_NOOP("cosanta-core", "Error reading from database, shutting down."),
 QT_TRANSLATE_NOOP("cosanta-core", "Error upgrading chainstate database"),
 QT_TRANSLATE_NOOP("cosanta-core", "Error upgrading evo database"),
-QT_TRANSLATE_NOOP("cosanta-core", "Error"),
 QT_TRANSLATE_NOOP("cosanta-core", "Error: A fatal internal error occurred, see debug.log for details"),
-QT_TRANSLATE_NOOP("cosanta-core", "Error: Disk space is low!"),
+QT_TRANSLATE_NOOP("cosanta-core", "Error: Disk space is low for %s"),
 QT_TRANSLATE_NOOP("cosanta-core", "Error: failed to add socket to epollfd (epoll_ctl returned error %s)"),
 QT_TRANSLATE_NOOP("cosanta-core", "Error: failed to add socket to kqueuefd (kevent returned error %s)"),
 QT_TRANSLATE_NOOP("cosanta-core", "Exceeded max tries."),
-QT_TRANSLATE_NOOP("cosanta-core", "Failed to clear fulfilled requests cache at"),
-QT_TRANSLATE_NOOP("cosanta-core", "Failed to clear governance cache at"),
-QT_TRANSLATE_NOOP("cosanta-core", "Failed to clear masternode cache at"),
+QT_TRANSLATE_NOOP("cosanta-core", "Failed to clear fulfilled requests cache at %s"),
+QT_TRANSLATE_NOOP("cosanta-core", "Failed to clear governance cache at %s"),
+QT_TRANSLATE_NOOP("cosanta-core", "Failed to clear masternode cache at %s"),
 QT_TRANSLATE_NOOP("cosanta-core", "Failed to commit EvoDB"),
 QT_TRANSLATE_NOOP("cosanta-core", "Failed to create backup %s!"),
 QT_TRANSLATE_NOOP("cosanta-core", "Failed to create backup, error: %s"),
 QT_TRANSLATE_NOOP("cosanta-core", "Failed to delete backup, error: %s"),
 QT_TRANSLATE_NOOP("cosanta-core", "Failed to find mixing queue to join"),
 QT_TRANSLATE_NOOP("cosanta-core", "Failed to listen on any port. Use -listen=0 if you want this."),
-QT_TRANSLATE_NOOP("cosanta-core", "Failed to load fulfilled requests cache from"),
-QT_TRANSLATE_NOOP("cosanta-core", "Failed to load governance cache from"),
-QT_TRANSLATE_NOOP("cosanta-core", "Failed to load masternode cache from"),
-QT_TRANSLATE_NOOP("cosanta-core", "Failed to load sporks cache from"),
+QT_TRANSLATE_NOOP("cosanta-core", "Failed to load fulfilled requests cache from %s"),
+QT_TRANSLATE_NOOP("cosanta-core", "Failed to load governance cache from %s"),
+QT_TRANSLATE_NOOP("cosanta-core", "Failed to load masternode cache from %s"),
+QT_TRANSLATE_NOOP("cosanta-core", "Failed to load sporks cache from %s"),
 QT_TRANSLATE_NOOP("cosanta-core", "Failed to rescan the wallet during initialization"),
 QT_TRANSLATE_NOOP("cosanta-core", "Failed to start a new mixing queue"),
 QT_TRANSLATE_NOOP("cosanta-core", "Found enough users, signing ( waiting %s )"),
@@ -198,13 +201,13 @@ QT_TRANSLATE_NOOP("cosanta-core", "Incompatible mode."),
 QT_TRANSLATE_NOOP("cosanta-core", "Incompatible version."),
 QT_TRANSLATE_NOOP("cosanta-core", "Incorrect -rescan mode, falling back to default value"),
 QT_TRANSLATE_NOOP("cosanta-core", "Incorrect or no genesis block found. Wrong datadir for network?"),
-QT_TRANSLATE_NOOP("cosanta-core", "Information"),
 QT_TRANSLATE_NOOP("cosanta-core", "Initialization sanity check failed. %s is shutting down."),
 QT_TRANSLATE_NOOP("cosanta-core", "Input is not valid."),
 QT_TRANSLATE_NOOP("cosanta-core", "Inputs vs outputs size mismatch."),
 QT_TRANSLATE_NOOP("cosanta-core", "Insufficient funds."),
 QT_TRANSLATE_NOOP("cosanta-core", "Invalid -onion address or hostname: '%s'"),
 QT_TRANSLATE_NOOP("cosanta-core", "Invalid -proxy address or hostname: '%s'"),
+QT_TRANSLATE_NOOP("cosanta-core", "Invalid P2P permission: '%s'"),
 QT_TRANSLATE_NOOP("cosanta-core", "Invalid amount for -%s=<amount>: '%s'"),
 QT_TRANSLATE_NOOP("cosanta-core", "Invalid amount for -discardfee=<amount>: '%s'"),
 QT_TRANSLATE_NOOP("cosanta-core", "Invalid amount for -fallbackfee=<amount>: '%s'"),
@@ -214,8 +217,6 @@ QT_TRANSLATE_NOOP("cosanta-core", "Invalid minimum number of spork signers speci
 QT_TRANSLATE_NOOP("cosanta-core", "Invalid netmask specified in -whitelist: '%s'"),
 QT_TRANSLATE_NOOP("cosanta-core", "Invalid script detected."),
 QT_TRANSLATE_NOOP("cosanta-core", "Invalid spork address specified with -sporkaddr"),
-QT_TRANSLATE_NOOP("cosanta-core", "It has been replaced by -disablegovernance."),
-QT_TRANSLATE_NOOP("cosanta-core", "Its replacement -disablegovernance has been forced instead."),
 QT_TRANSLATE_NOOP("cosanta-core", "Keypool ran out, please call keypoolrefill first"),
 QT_TRANSLATE_NOOP("cosanta-core", "Last queue was created too recently."),
 QT_TRANSLATE_NOOP("cosanta-core", "Last successful action was too recent."),
@@ -243,12 +244,14 @@ QT_TRANSLATE_NOOP("cosanta-core", "Not enough file descriptors available."),
 QT_TRANSLATE_NOOP("cosanta-core", "Not enough funds to mix."),
 QT_TRANSLATE_NOOP("cosanta-core", "Not in the Masternode list."),
 QT_TRANSLATE_NOOP("cosanta-core", "Prune cannot be configured with a negative value."),
+QT_TRANSLATE_NOOP("cosanta-core", "Prune mode is incompatible with -blockfilterindex."),
 QT_TRANSLATE_NOOP("cosanta-core", "Prune mode is incompatible with -disablegovernance=false."),
 QT_TRANSLATE_NOOP("cosanta-core", "Prune mode is incompatible with -txindex."),
 QT_TRANSLATE_NOOP("cosanta-core", "Pruning blockstore..."),
 QT_TRANSLATE_NOOP("cosanta-core", "Reducing -maxconnections from %d to %d, because of system limitations."),
 QT_TRANSLATE_NOOP("cosanta-core", "Replaying blocks..."),
 QT_TRANSLATE_NOOP("cosanta-core", "Rescanning..."),
+QT_TRANSLATE_NOOP("cosanta-core", "Section [%s] is not recognized."),
 QT_TRANSLATE_NOOP("cosanta-core", "Session not complete!"),
 QT_TRANSLATE_NOOP("cosanta-core", "Session timed out."),
 QT_TRANSLATE_NOOP("cosanta-core", "Signing transaction failed"),
@@ -262,6 +265,7 @@ QT_TRANSLATE_NOOP("cosanta-core", "Synchronization finished"),
 QT_TRANSLATE_NOOP("cosanta-core", "Synchronizing blockchain..."),
 QT_TRANSLATE_NOOP("cosanta-core", "Synchronizing governance objects..."),
 QT_TRANSLATE_NOOP("cosanta-core", "The source code is available from %s."),
+QT_TRANSLATE_NOOP("cosanta-core", "The specified config file %s does not exist\n"),
 QT_TRANSLATE_NOOP("cosanta-core", "The transaction amount is too small to pay the fee"),
 QT_TRANSLATE_NOOP("cosanta-core", "The wallet will avoid paying less than the minimum relay fee."),
 QT_TRANSLATE_NOOP("cosanta-core", "This is expected because you are running a pruned node."),
@@ -275,33 +279,29 @@ QT_TRANSLATE_NOOP("cosanta-core", "Transaction fees are too high."),
 QT_TRANSLATE_NOOP("cosanta-core", "Transaction has too long of a mempool chain"),
 QT_TRANSLATE_NOOP("cosanta-core", "Transaction must have at least one recipient"),
 QT_TRANSLATE_NOOP("cosanta-core", "Transaction not valid."),
-QT_TRANSLATE_NOOP("cosanta-core", "Transaction too large for fee policy"),
 QT_TRANSLATE_NOOP("cosanta-core", "Transaction too large"),
 QT_TRANSLATE_NOOP("cosanta-core", "Trying to connect..."),
 QT_TRANSLATE_NOOP("cosanta-core", "Unable to bind to %s on this computer (bind returned error %s)"),
 QT_TRANSLATE_NOOP("cosanta-core", "Unable to bind to %s on this computer. %s is probably already running."),
+QT_TRANSLATE_NOOP("cosanta-core", "Unable to create the PID file '%s': %s"),
 QT_TRANSLATE_NOOP("cosanta-core", "Unable to generate initial keys"),
 QT_TRANSLATE_NOOP("cosanta-core", "Unable to locate enough mixed funds for this transaction."),
 QT_TRANSLATE_NOOP("cosanta-core", "Unable to locate enough non-denominated funds for this transaction."),
 QT_TRANSLATE_NOOP("cosanta-core", "Unable to sign spork message, wrong key?"),
 QT_TRANSLATE_NOOP("cosanta-core", "Unable to start HTTP server. See debug log for details."),
+QT_TRANSLATE_NOOP("cosanta-core", "Unknown -blockfilterindex value %s."),
 QT_TRANSLATE_NOOP("cosanta-core", "Unknown network specified in -onlynet: '%s'"),
 QT_TRANSLATE_NOOP("cosanta-core", "Unknown response."),
 QT_TRANSLATE_NOOP("cosanta-core", "Unknown state: id = %u"),
-QT_TRANSLATE_NOOP("cosanta-core", "Unsupported argument -benchmark ignored, use -debug=bench."),
-QT_TRANSLATE_NOOP("cosanta-core", "Unsupported argument -debugnet ignored, use -debug=net."),
-QT_TRANSLATE_NOOP("cosanta-core", "Unsupported argument -tor found, use -onion."),
 QT_TRANSLATE_NOOP("cosanta-core", "Unsupported logging category %s=%s."),
 QT_TRANSLATE_NOOP("cosanta-core", "Upgrading UTXO database"),
+QT_TRANSLATE_NOOP("cosanta-core", "Upgrading txindex database"),
 QT_TRANSLATE_NOOP("cosanta-core", "User Agent comment (%s) contains unsafe characters."),
 QT_TRANSLATE_NOOP("cosanta-core", "Verifying blocks..."),
 QT_TRANSLATE_NOOP("cosanta-core", "Verifying wallet(s)..."),
 QT_TRANSLATE_NOOP("cosanta-core", "Very low number of keys left: %d"),
-QT_TRANSLATE_NOOP("cosanta-core", "Wallet %s resides outside wallet directory %s"),
 QT_TRANSLATE_NOOP("cosanta-core", "Wallet is locked."),
 QT_TRANSLATE_NOOP("cosanta-core", "Wallet needed to be rewritten: restart %s to complete"),
-QT_TRANSLATE_NOOP("cosanta-core", "Warning"),
-QT_TRANSLATE_NOOP("cosanta-core", "Warning: %s is deprecated, please use %s instead"),
 QT_TRANSLATE_NOOP("cosanta-core", "Warning: can't use %s and %s together, will prefer %s"),
 QT_TRANSLATE_NOOP("cosanta-core", "Warning: incorrect parameter %s, path must exist! Using default path."),
 QT_TRANSLATE_NOOP("cosanta-core", "Warning: unknown new rules activated (versionbit %i)"),
@@ -312,7 +312,6 @@ QT_TRANSLATE_NOOP("cosanta-core", "You can not disable governance validation on 
 QT_TRANSLATE_NOOP("cosanta-core", "You can not start a masternode with wallet enabled."),
 QT_TRANSLATE_NOOP("cosanta-core", "You need to rebuild the database using -reindex to change -addressindex"),
 QT_TRANSLATE_NOOP("cosanta-core", "You need to rebuild the database using -reindex to change -spentindex"),
-QT_TRANSLATE_NOOP("cosanta-core", "You need to rebuild the database using -reindex to change -txindex"),
 QT_TRANSLATE_NOOP("cosanta-core", "Your entries added successfully."),
 QT_TRANSLATE_NOOP("cosanta-core", "Zapping all transactions from wallet..."),
 QT_TRANSLATE_NOOP("cosanta-core", "no mixing available."),
