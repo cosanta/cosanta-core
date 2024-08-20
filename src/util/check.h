bitcoin/bitcoin

merge bitcoin#18740: Remove g_rpc_node global

src/util/check.h
@@ -5,6 +5,10 @@
 #ifndef BITCOIN_UTIL_CHECK_H
 #define BITCOIN_UTIL_CHECK_H
 
+#if defined(HAVE_CONFIG_H)
+#include <config/dash-config.h>
+#endif
+
 #include <tinyformat.h>
 
 #include <stdexcept>

cosanta/cosanta-core

src/util/check.h
@@ -5,6 +5,10 @@
 #ifndef BITCOIN_UTIL_CHECK_H
 #define BITCOIN_UTIL_CHECK_H
 
+#if defined(HAVE_CONFIG_H)
+#include <config/cosanta-config.h>
+#endif
+
 #include <tinyformat.h>
 
 #include <stdexcept>