01aa0a0529079332cbd17128ea16d41186ccbdc1
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2026-05-18 14:01:23 +0300

    build: brand command and package metadata for Cosanta

--- test/lint/lint-cppcheck-cosanta.sh
+++ test/lint/lint-cppcheck-cosanta.sh
@@ -60,7 +60,7 @@ IGNORED_WARNINGS=(
     "unusedStructMember"
 )
 
-# We should attempt to update this with all dash specific code
+# We should attempt to update this with all Cosanta-specific code
 FILES=$(git ls-files -- $(cat test/util/data/non-backported.txt))
 
 
